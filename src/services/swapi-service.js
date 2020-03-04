d033025d2b79c1dac12c7648cfd9398977fdc7c9
user-rq <dina.bolshakova@requestum.com>
2020-03-04 18:44:56 +0200

    started Star Wars project

---
+++ src/services/swapi-service.js
@@ -0,0 +1,79 @@
+export default class SwapiService {
+    _apiBase = 'https://swapi.co/api'; // private
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}` + `, 
+            received ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
+
+    async getAllPlanets() {
+        const res = this.getResource(`/planets/`);
+        return res.results.map(this._transformPlanet);
+    }
+
+    async getAllStarships() {
+        const res = this.getResource(`/starships/`);
+        return res.results;
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}`);
+    }
+
+    async getPlanet(id) {
+        const planet = await this.getResource(`/planets/${id}`);
+        return this._transformPlanet(planet);
+    }
+
+    getStarship(id) {
+        return this.getResource(`/starships/${id}`);
+    }
+
+    _extractId(item) {
+        const idRegExp = /\/([0-9]*)\/$/;
+        return item.url.match(idRegExp)[1];
+    }
+
+    _transformPlanet(planet) {
+        return {
+            id: this._extractId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            diameter: planet.diameter
+        }
+    }
+
+    _transformStarship(starship){
+        return {
+            id: this._extractId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+
+        }
+    }
+
+    _transformPerson(person){
+        return {
+            id: this._extractId(person),
+            name: person.name,
+            gender:person.gender,
+            birthYear: person.birthYear,
+            eyeColor: person.eyeColor
+        }
+    }
+}
+
+
